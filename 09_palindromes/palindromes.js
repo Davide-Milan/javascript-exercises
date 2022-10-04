cfca8e3a2e4048ccb9823eca044b5bc4be4be696
Davide-Milan <davidemilan27@gmail.com>
2022-10-04 18:24:03 +0200

    Finish exercise 9 and do exercises 10, 11, and 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,12 @@
 const palindromes = function (text) {
-    text.toLowerCase();
+    text = text.toLowerCase();
     text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-    console.log(text);
+    text = text.replaceAll(" ","");
+    console.log("AAAAAAAAAAAAAA ", text);
+    for(let pos = 0; pos < text.length; pos++){
+        if (text[pos] !== text[text.length-pos-1] ) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
